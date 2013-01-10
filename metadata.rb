24021948a178ff499244c3bf8a046ff9c06a2c57
Brett Weaver <brett@weav.net>
2013-01-10 08:01:19 -0800

    adding metadata

---
+++ metadata.rb
@@ -0,0 +1,13 @@
+name             's3cmd-cookbook'
+maintainer       'Brett Weaver'
+maintainer_email 'brett_weaver@intuit.com'
+license          'Apache 2.0'
+description      'Configures s3cmd'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+recipe 's3cmd', 'Configures s3cmd'
+
+%w{redhat centos}.each do |os|
+  supports os
+end
