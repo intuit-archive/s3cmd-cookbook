5d560e2a669faf9c9b80f573677192ba01fbcfd4
Brett Weaver <brett@weav.net>
2013-03-19 07:11:36 -0700

    adding upplioad lwrp

--- providers/file.rb
+++ providers/file.rb
@@ -1,12 +1,13 @@
 action :download do
 
-  file_name = new_resource.file_name
-  bucket = new_resource.bucket
+  file_name   = new_resource.file_name
+  bucket      = new_resource.bucket
   object_name = new_resource.object_name
-  object_name.slice!(0) if object_name.start_with? '/'
-  force = new_resource.force ? '--force' : ''
+  force       = new_resource.force ? '--force' : ''
 
-  execute "Downloading #{file_name} from s3" do
+  object_name.slice!(0) if object_name.start_with? '/'
+
+  execute "Downloading s3://#{bucket}/#{object_name} from s3 to #{file_name}" do
     command "s3cmd #{force} get s3://#{bucket}/#{object_name} #{file_name}"
   end
 
@@ -14,6 +15,20 @@ action :download do
     owner new_resource.owner
     group new_resource.group
     mode new_resource.mode
+  end
+
+end
+
+action :upload do
+
+  file_name   = new_resource.file_name
+  bucket      = new_resource.bucket
+  object_name = new_resource.object_name
+  acl_public  = new_resource.public ? '--acl-public' : ''
+  force       = new_resource.force ? '--force' : ''
+
+  execute "Downloading #{file_name} to s3://#{bucket}/#{object_name}" do
+    command "s3cmd #{force} #{acl_public} put #{file_name} s3://#{bucket}/#{object_name}"
   end
 
 end
