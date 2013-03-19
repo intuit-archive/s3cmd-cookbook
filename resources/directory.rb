46e4d22e46c94352d6f3e123fd2dcaaf0dafc5e1
Brett Weaver <brett@weav.net>
2013-03-19 14:00:31 -0700

    removing directory cookbook for now

--- resources/directory.rb
+++
@@ -1,7 +0,0 @@
-actions :sync_to_s3
-
-attribute :directory, :kind_of => String, :name_attribute => true
-attribute :bucket, :kind_of => String
-attribute :path, :kind_of => String
-attribute :force, :kind_of => [ TrueClass, FalseClass ], :default => true
-attribute :public, :kind_of => [ TrueClass, FalseClass ], :default => false
