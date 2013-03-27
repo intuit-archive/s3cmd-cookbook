9e3619dd5a5b048115b4e6fc75c89e63c59abdbe
Brett Weaver <brett@weav.net>
2013-03-27 08:18:55 -0700

    adding validation file exists

--- providers/file.rb
+++ providers/file.rb
@@ -18,12 +18,17 @@ action :download do
 end
 
 action :upload do
+  file_name = new_resource.file_name
+
+  raise "File #{file_name} not found." unless File.exists? file_name
+
   cmd = Upload.new.command :headers     => new_resource.headers,
-                           :file_name   => new_resource.file_name,
+                           :file_name   => file_name,
                            :bucket      => new_resource.bucket,
                            :object_name => new_resource.object_name,
                            :acl_public  => new_resource.acl_public,
                            :force       => new_resource.force
+
 
   execute "Uploading to S3 with cmd: '#{cmd}'" do
     command cmd
