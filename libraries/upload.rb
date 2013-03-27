a3ad0431cffa00324346e75ebcba77796ae0e7b9
Brett Weaver <brett@weav.net>
2013-03-27 07:08:12 -0700

    tweaked class

--- libraries/upload.rb
+++ libraries/upload.rb
@@ -1,24 +1,25 @@
 class Upload
 
   def command(args)
+    @headers    = args[:headers]
+
     acl_public  = args[:acl_public] ? '--acl-public' : ''
     bucket      = args[:bucket]
     object_name = args[:object_name]
-    headers     = args[:headers]
     file_name   = args[:file_name]
     force       = args[:force] ? '--force' : ''
 
     object_name[0] = '' if object_name[0] == '/'
 
     cmd =  "s3cmd #{force} #{acl_public} "
-    cmd << header_args(headers)
+    cmd << headers
     cmd << " put #{file_name} s3://#{bucket}/#{object_name}"
 
     cmd
   end
 
-  def header_args(headers)
-    headers.each_pair.map {|k,v| "--add-header='#{k}':'#{v}'"}.join(' ')
+  def headers
+    @headers.each_pair.map {|k,v| "--add-header='#{k}':'#{v}'"}.join(' ')
   end
 
 end
